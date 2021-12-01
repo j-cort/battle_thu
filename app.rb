27e76b4868d8c5db1d76b6762fb6186dad7cf397
j-cort <81960011+j-cort@users.noreply.github.com>
2021-12-01 15:18:15 +0000

    Enter Player Names Form

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
-    "Hello Battle!"
+    erb(:index)
+  end
+
+  post '/names' do
+   @player_1 = params[:player_1]
+   @player_2 = params[:player_2]
+   erb(:play)
   end
 
   # # Start the server if this file is executed directly (do not change the line below)
--- spec/features/testing_infastructure_spec.rb
+++ spec/features/testing_infastructure_spec.rb
@@ -1,6 +1,20 @@
 feature 'Accessing the page' do 
+  
   scenario 'Testing infastructure working' do 
     visit '/'
     expect(page).to have_content 'Hello Battle!'
   end
+
+end
+
+feature 'Entering Player Names' do 
+  
+  scenario 'Testing infastructure working' do 
+    visit '/'
+    fill_in 'player_1', with: 'James'
+    fill_in 'player_2', with: 'Chris'
+    click_button 'Submit'
+    expect(page).to have_content 'James vs Chris'
+  end
+
 end
